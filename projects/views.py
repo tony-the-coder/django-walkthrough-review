a10b388c9a612593ade5c648c39902769ea3cf9a
Tony Aldea <tonythecoder@outlook.com>
2025-02-05 16:29:48 -0500

    Updating db views

--- projects/views.py
+++ projects/views.py
@@ -1,24 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
-# projectsList = [
-#     {
-#         'id': '1',
-#         'title': 'Ecommerce Website',
-#         'description': 'Fully functional ecommerce website'
-#     },
-#     {
-#         'id': '2',
-#         'title': 'Portfolio Website',
-#         'description': 'A personal website to write articles and display work'
-#     },
-#     {
-#         'id': '3',
-#         'title': 'Social Network',
-#         'description': 'An open source project built by the community'
-#     }
-# ]
+from .forms import ProjectForm
 
 def projects(request):
     projects = Project.objects.all()
@@ -27,6 +10,14 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    tags = projectObj.tags.all()
+    # tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html', {'project': projectObj})
 # Create your views here.
+
+
+# Starting to work on the forms
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
